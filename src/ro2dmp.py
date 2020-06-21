c7cc44ed5a963f5c034ba81818c8fb17d8eb98cc
BrennerG <gabrielbrbr@gmail.com>
2020-06-21 19:51:31 +0200

    MOD ro2dmp: Transformations complete

--- src/ro2dmp.py
+++ src/ro2dmp.py
@@ -1,5 +1,6 @@
 import json
 import sys
+from datetime import date
 
 class DMP_Constructor:
 
@@ -24,19 +25,47 @@ class DMP_Constructor:
             if key in pos:
                 return pos[key]
             else:
-                # TODO more precise location output
                 print("WARNING: could not find", key)
     
-    # TODO add-method for null-checking
+    def clean(self, dictionary:dict):
+        cleaned = {}
 
-    def construct(self):
+        if type(dictionary) == dict:
+            for k in dictionary:
+                if dictionary[k] != None:
+                    cleaned[k] = dictionary[k]
+
+        elif type(dictionary) == list:
+            for dic in dictionary:
+                self.clean(dic)
+
+        return cleaned
+
+    def identify(self, identifier:str):
+        identifiers = ['doi', 'orcid', 'handle', 'ark', 'isni', 'openid']
+
+        if identifier == None:
+            return None
+
+        for ident in identifiers:
+            if ident in identifier:
+                return ident
+            elif identifier.startswith('http') or identifier.startswith('www'):
+                return 'url'
+                
+        return 'other'
+
+    def construct(self, clean:bool=False):
         self.dmp = self.dmp_skeleton()
-        # TODO self.set_base_attributes()
+        self.set_base_attributes()
         person_list = self.extract_people()
         self.dmp['dmp']['contact']  = person_list[0]
         self.dmp['dmp']['contributor'] = person_list[1:]
         self.dmp['dmp']['dataset'] = self.extract_dataset()
         self.dmp['dmp']['project'] = self.extract_project()
+
+        if clean:
+            self.dmp['dmp'] = self.clean(self.dmp['dmp'])
     
     def dmp_skeleton(self):
         return {
@@ -50,32 +79,25 @@ class DMP_Constructor:
                 'modified' : None,
                 'ethical_issues_description' : None,
                 'ethical_issues_exist' : None,
-
                 # Big ones
                 'contact' : {},
                 'contributor' : [],
-                # 'cost' : [], TODO is this really lost?
+                'cost' : [],
                 'project' : [],
                 'dataset' : []
             }
         }
 
     def set_base_attributes(self):
-        # find root entity
-        for entity in self.RO_Crate:
-            if '@type' in entity and entity['@type'] == "CreativeWork":
-                pass
-
-        # 'title' : None,
-        # 'dmp_id' : None,
-        # 'created' : None,
-        # 'description' : None,
-        # 'language' : None,
-        # 'modified' : None,
-        # 'ethical_issues_description' : None,
-        # 'ethical_issues_exist' : None,
-        pass
-
+        created = date.today().strftime("%Y/%m/%d")
+        self.dmp['dmp']['dmp_id'] = "PLEASE REGISTER A DOI"
+        self.dmp['dmp']['title'] = "Machine-Actionable Data Management Plan"
+        self.dmp['dmp']['description'] = "This is a machine-generated maDMP. Please fill out missing fields according to RDA maDMP standards."
+        self.dmp['dmp']['created'] = created
+        self.dmp['dmp']['modified'] = created
+        self.dmp['dmp']['language'] = None
+        self.dmp['dmp']['ethical_issues_exist'] = None
+        self.dmp['dmp']['ethical_issues_description'] = None
 
     def extract_people(self):
         contributors = []
@@ -83,8 +105,8 @@ class DMP_Constructor:
             if '@type' in entity and entity['@type'] == 'Person':
                 contributors.append({
                     'contact_id' : {
-                        'type' : "orcid", # TODO check wether this really is orcid
-                        'identifier' : self.check('@id', entity)
+                        'identifier' : self.check('@id', entity),
+                        'type' : self.identify(self.check('@id', entity))
                     },
                     'mbox' : self.check('email', entity),
                     'name' : self.check('name', entity)
@@ -96,19 +118,24 @@ class DMP_Constructor:
         projects = []
         for entity in self.RO_Crate:
             if '@type' in entity and (entity['@type'] == 'Project' or (entity['@type'] == 'Organization' and 'funder' in entity)):
-                # FROM RO-CRATE @id, @type, description, identifier, name
-                # MISSING FROM DMP start, funding_status, grant_id
                 projects.append({
                     'title' : self.check('title', entity),
                     'description' : self.check('description', entity),
+                    'start' : None,
                     'funding' : {
                         'funder_id' : {
                             'identifier' : self.check('@id', entity, 'funder'),
-                            'type' : 'doi' # TODO create checking function
+                            'type' : self.identify(self.check('@id', entity, 'funder'))
                         }
+                    },
+                    'funding_status' : None,
+                    'grant_id' : {
+                        'identifier' : None,
+                        'type' : None
                     }
                 })
 
+        return projects
 
     def extract_dataset(self):
         data = []
@@ -119,16 +146,15 @@ class DMP_Constructor:
                     'type' : self.check("encodingFormat", entity),
                     'dataset_id' : {
                         'identifier' : self.check("@id", entity),
-                        'type' : 'doi' # TODO check this properly
+                        'type' : self.identify(self.check('@id', entity))
                     },
                     'description' : self.check("description", entity),
                     'issued' : self.check('datePublished', entity),
-                    'keyword' : self.check("keywords", entity), # TODO unsure if "keywords" is name
+                    'keyword' : self.check("keywords", entity),
                     'language' : self.check("Language", entity),
-                    #  # TODO can the following even be retrieved ?
-                    # 'personal_data' : None,
-                    # 'preservation_statement' : None,
-                    # 'sensitive_data' : None
+                    'personal_data' : None,
+                    'preservation_statement' : None,
+                    'sensitive_data' : None
                 })
 
         return data
@@ -138,8 +164,8 @@ if __name__ == "__main__":
     ro1 = 'samples/ro-crate-metadata1.jsonld' 
     ro2 = 'samples/ro-crate-metadata2.jsonld' 
     ro3 = 'samples/ro-crate-metadata3.jsonld' 
-    out = 'transformation3.jsonld'
 
+    out = 'transformation3.jsonld'
     DC = DMP_Constructor(ro3)
     DC.construct()
     DC.write(out)
